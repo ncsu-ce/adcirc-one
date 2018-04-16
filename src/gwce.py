51f85ae7d50cc83d548c7073be6be1317dcb7542
Tristan <atdyer@ncsu.edu>
2018-04-16 14:13:57 -0400

    More gwce work

--- src/gwce.py
+++ src/gwce.py
@@ -8,7 +8,55 @@ def initialize_gwce(num_nodes):
     lhs = np.zeros((num_nodes, num_nodes), dtype=float)
 
 
-def build_gwce_lhs(lhs, dt, nodes, alpha, h):
+def build_gwce_rhs(rhs, dt, nodes, alpha, z_current, z_previous, tau, q, h):
+
+    num_nodes = nodes.shape[0]
+    num_elements = num_nodes - 1
+    alpha_1 = alpha[0]
+    alpha_2 = alpha[1]
+    alpha_3 = alpha[2]
+
+    for element in range(num_elements):
+
+        n1 = element
+        n2 = element + 1
+
+        le = nodes[n2] - nodes[n1]
+        dz1 = z_current[n1] - z_previous[n1]
+        dz2 = z_current[n2] - z_previous[n2]
+
+        # (5) contribution
+        tau_avg = (tau[n1] + tau[n2]) / 2
+        coeff = tau_avg / 3
+        rhs[n1] += 2 * coeff * q[n1]
+        rhs[n1] += coeff * q[n2]
+        rhs[n2] += coeff * q[n1]
+        rhs[n2] += 2 * coeff * q[n2]
+
+        # (6) contribution
+        coeff = le / (6 * dt)
+        rhs[n1] += 2 * coeff * dz1
+        rhs[n1] += coeff * dz2
+        rhs[n2] += coeff * dz1
+        rhs[n2] += 2 * coeff * dz2
+
+        # (7) contribution
+        coeff = 1 / (12 * dt)
+        rhs[n1] -= 2 * coeff * dz1
+        rhs[n1] -= coeff * dz2
+        rhs[n2] -= coeff * dz1
+        rhs[n2] -= 2 * coeff * dz2
+
+        # (8) contribution
+        h_avg = (h[n1] + h[n2]) / 2
+        coeff = g * h_avg / le
+        rhs[n1] -= coeff * ((alpha_1 + alpha_2) * z_current[n1] + alpha_3 * z_previous[n1])
+        rhs[n1] += coeff * ((alpha_1 + alpha_2) * z_current[n2] + alpha_3 * z_previous[n2])
+        rhs[n2] -= coeff * ((alpha_1 + alpha_2) * z_current[n1] + alpha_3 * z_previous[n1])
+        rhs[n2] += coeff * ((alpha_1 + alpha_2) * z_current[n2] + alpha_3 * z_previous[n2])
+
+
+def build_gwce_lhs(lhs, dt, nodes, alpha, h, tau):
 
     num_nodes = nodes.shape[0]
     num_elements = num_nodes - 1
@@ -21,19 +69,27 @@ def build_gwce_lhs(lhs, dt, nodes, alpha, h):
 
         le = nodes[n2] - nodes[n1]      # element length
 
-        # dddz/dt contribution
-        dddz = le / (6 * dt**2)
-        lhs[n1][n1] += 2 * dddz
-        lhs[n1][n2] += dddz
-        lhs[n2][n1] += dddz
-        lhs[n2][n2] += 2 * dddz
+        # (1) contribution
+        coeff = le / (6 * dt**2)
+        lhs[n1][n1] += 2 * coeff
+        lhs[n1][n2] += coeff
+        lhs[n2][n1] += coeff
+        lhs[n2][n2] += 2 * coeff
+
+        # (2) contribution
+        tau_avg = (tau[n1] + tau[n2]) / 2
+        coeff = (tau_avg + le) / (12 * dt)
+        lhs[n1][n1] += 2 * coeff
+        lhs[n1][n2] += coeff
+        lhs[n2][n1] += coeff
+        lhs[n2][n2] += 2 * coeff
 
         # g*h*dz/dx contribution
         h_avg = (h[n1] + h[n2]) / 2
-        dzdx = g * alpha_1 * h_avg / le
-        lhs[n1][n1] += dzdx
-        lhs[n1][n2] -= dzdx
-        lhs[n2][n1] -= dzdx
-        lhs[n2][n2] += dzdx
+        coeff = g * alpha_1 * h_avg / le
+        lhs[n1][n1] += coeff
+        lhs[n1][n2] -= coeff
+        lhs[n2][n1] -= coeff
+        lhs[n2][n2] += coeff
 
     return lhs
--- src/simple.py
+++ src/simple.py
@@ -5,6 +5,7 @@ num_elements = 3
 width = 3
 tau_0 = 1
 g = 9.81
+dt = 0.5
 
 # Constants
 num_nodes = num_elements + 1
@@ -15,36 +16,34 @@ element_width = width / num_elements
 # x-coordinates of nodes
 x = np.linspace(0, width, num=num_nodes, endpoint=True)
 
+# Matrices
+#
+# Each will be (n x 3) for past, present, and future timesteps
+#
 # Initialize depth to one meter
-h = np.ones(num_nodes)          # bathymetric depth
-zeta = np.zeros(num_nodes)      # free-surface departure from geoid
-H = h + zeta                    # total water column thickness
+h = np.ones((num_nodes, 3))     # bathymetric depth
+z = np.zeros((num_nodes, 3))    # free-surface departure from geoid
+H = h + z                       # total water-column thickness
+U = np.zeros((num_nodes, 3))    # x-velocity
+Q = H * U                       # x-flux
 
-K = np.zeros((num_nodes, num_nodes))
-M = np.zeros((num_nodes, num_nodes))
-F = np.zeros((num_nodes, num_nodes))
-
-for node in range(num_nodes):
-
-    # Term (1) contribution
-    if node - 1 >= 0:
-        K[node][node-1] += 0.5 * tau_0
-    if node + 1 < num_nodes:
-        K[node][node+1] -= 0.5 * tau_0
-
-    # Term (2) contribution
-    if node - 1 >= 0:
-
-        h_bar_l = (h[node-1] + h[node]) / 2.0
-        K[node][node-1] -= g * h_bar_l
-        K[node][node] += g * h_bar_l
-
-    if node + 1 < num_nodes:
-
-        h_bar_r = (h[node] + h[node+1]) / 2.0
-        K[node][node] -= g * h_bar_r
-        K[node][node+1] += g * h_bar_r
-
-
-print(x)
+print('h =')
+print(h)
+print('z =')
+print(z)
+print('H =')
 print(H)
+print('U =')
+print(U)
+print('Q =')
+print(Q)
+
+
+# zeta = np.zeros(num_nodes)      # free-surface departure from geoid
+# H = h + zeta                    # total water column thickness
+
+# K = np.zeros((num_nodes, num_nodes))
+# M = np.zeros((num_nodes, num_nodes))
+# F = np.zeros((num_nodes, num_nodes))
+
+
